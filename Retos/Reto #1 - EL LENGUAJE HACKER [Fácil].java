195d0d074a6c7737d8f745af9a52e44ec984d481
Roberto Ruiz <30808852+rruisan@users.noreply.github.com>
2023-07-29 13:30:45 +0200

    Reto #1 - EL LENGUAJE HACKER [Fácil].java

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil].java
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil].java
@@ -12,65 +12,64 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-        // Create a dictionary mapping Spanish characters to their "leet" equivalents
+        	// Create a dictionary mapping Spanish characters to their "leet" equivalents
 		Map<Character, String> spanishLeetDictionary = createSpanishLeetDictionary();
 		
 		// Input phrase to be converted
-	    String phrase = "Esto es una frase de prueba";
+	   	 String phrase = "Esto es una frase de prueba";
 	    
-	    // Convert the phrase to a vector of characters in lower case
-	    char[] phrase_vectorized = phrase.toLowerCase().toCharArray();
+	    	// Convert the phrase to a vector of characters in lower case
+	    	char[] phrase_vectorized = phrase.toLowerCase().toCharArray();
 	    
-	    // Convert the phrase using the "leet" dictionary
-	    String phrase_converted = convertPhrase(phrase_vectorized, spanishLeetDictionary);
-	    System.out.println(phrase_converted);
+		// Convert the phrase using the "leet" dictionary
+		String phrase_converted = convertPhrase(phrase_vectorized, spanishLeetDictionary);
+		System.out.println(phrase_converted);
 	}
 	
 	public static String convertPhrase(char[] phrase_vectorized, Map<Character, String> spanishLeetDictionary){
-	    String phrase_converted = "";
-	    
-	     for (char c : phrase_vectorized){
-	        if (c != ' '){
-	            phrase_converted += spanishLeetDictionary.get(c);
-	        }
-	        else{
-	            phrase_converted += " ";
-	        }
-	    }
-	    
-	    
-	    return phrase_converted;
+		String phrase_converted = "";
+		
+		for (char c : phrase_vectorized){
+			if (c != ' '){
+				phrase_converted += spanishLeetDictionary.get(c);
+			}
+			else{
+				phrase_converted += " ";
+			}
+		}
+		
+		return phrase_converted;
 	}
 	
 	public static Map<Character, String> createSpanishLeetDictionary() {
-        Map<Character, String> spanishLeetDictionary = new HashMap<>();
-        spanishLeetDictionary.put('a', "4");
-        spanishLeetDictionary.put('b', "8");
-        spanishLeetDictionary.put('c', "(");
-        spanishLeetDictionary.put('d', "|)");
-        spanishLeetDictionary.put('e', "3");
-        spanishLeetDictionary.put('f', "|=");
-        spanishLeetDictionary.put('g', "9");
-        spanishLeetDictionary.put('h', "|-|");
-        spanishLeetDictionary.put('i', "!");
-        spanishLeetDictionary.put('j', "_|");
-        spanishLeetDictionary.put('k', "|<");
-        spanishLeetDictionary.put('l', "1");
-        spanishLeetDictionary.put('m', "|\\/|");
-        spanishLeetDictionary.put('n', "|\\|");
-        spanishLeetDictionary.put('o', "0");
-        spanishLeetDictionary.put('p', "|>");
-        spanishLeetDictionary.put('q', "0,");
-        spanishLeetDictionary.put('r', "|2");
-        spanishLeetDictionary.put('s', "$");
-        spanishLeetDictionary.put('t', "7");
-        spanishLeetDictionary.put('u', "|_|");
-        spanishLeetDictionary.put('v', "\\/");
-        spanishLeetDictionary.put('w', "\\/\\/");
-        spanishLeetDictionary.put('x', "><");
-        spanishLeetDictionary.put('y', "`/");
-        spanishLeetDictionary.put('z', "2");
-
-        return spanishLeetDictionary;
-    }
+		Map<Character, String> spanishLeetDictionary = new HashMap<>();
+		spanishLeetDictionary.put('a', "4");
+		spanishLeetDictionary.put('b', "8");
+		spanishLeetDictionary.put('c', "(");
+		spanishLeetDictionary.put('d', "|)");
+		spanishLeetDictionary.put('e', "3");
+		spanishLeetDictionary.put('f', "|=");
+		spanishLeetDictionary.put('g', "9");
+		spanishLeetDictionary.put('h', "|-|");
+		spanishLeetDictionary.put('i', "!");
+		spanishLeetDictionary.put('j', "_|");
+		spanishLeetDictionary.put('k', "|<");
+		spanishLeetDictionary.put('l', "1");
+		spanishLeetDictionary.put('m', "|\\/|");
+		spanishLeetDictionary.put('n', "|\\|");
+		spanishLeetDictionary.put('o', "0");
+		spanishLeetDictionary.put('p', "|>");
+		spanishLeetDictionary.put('q', "0,");
+		spanishLeetDictionary.put('r', "|2");
+		spanishLeetDictionary.put('s', "$");
+		spanishLeetDictionary.put('t', "7");
+		spanishLeetDictionary.put('u', "|_|");
+		spanishLeetDictionary.put('v', "\\/");
+		spanishLeetDictionary.put('w', "\\/\\/");
+		spanishLeetDictionary.put('x', "><");
+		spanishLeetDictionary.put('y', "`/");
+		spanishLeetDictionary.put('z', "2");
+		
+		return spanishLeetDictionary;
+    	}
 }
